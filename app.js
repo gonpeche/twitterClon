43ce49aa585d6507b289997685d8b2956e30564e
gonpeche <espeche.g@gmail.com>
2018-09-19 12:30:10 -0300

    Cambios

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require( 'express' );
+
 const app = express(); 
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
@@ -19,9 +21,9 @@ app.set('view engine', 'html'); // hace que res.render funcione con archivos htm
 app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
 nunjucks.configure('views', {noCache: true}); // apunta a nunjucks al directorio correcto para los templates
 
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
 
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
