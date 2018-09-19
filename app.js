f85b5f0d21c61cf80ac39528229945254f878561
gonpeche <espeche.g@gmail.com>
2018-09-19 10:20:30 -0300

    express & logger middleware

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require( 'express' );
+const app = express(); 
+
+app.use(function(req,res,next){
+    console.log('hola')    
+    next();
+})
+
+app.use('/special*', function(req,res,next){
+    res.send('<h1> Lugar especial </h1>') 
+    next();
+})
+
+app.listen('3000')
+
+
+
