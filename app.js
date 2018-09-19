4ba9236f500fe78dbdbe4af79712c476458a959c
gonpeche <espeche.g@gmail.com>
2018-09-19 15:31:56 -0300

    cambios

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const routes = require('./routes');
 app.use('/', routes);
 app.use(express.static('./public')); // sirve los files de tu PC
 
-
 app.set('view engine', 'html'); // hace que res.render funcione con archivos html
 app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
-
+nunjucks.configure('views', {noCache: true}); // apunta a nunjucks al directorio correcto para los templates
 
 // nunjucks.render('index.html', locals, function (err, output) {
 //     console.log(output);
